b90d49432b810cb4a25bf11ebc934468832782bf
whit <whit@40f73b84-6177-4a8e-8c61-7eee4b1974ed>
2007-11-27 21:14:31 +0000

    add svn to version number
    git-svn-id: https://svn.openplans.org/svn/PoachEggs/trunk@11284 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.3'
+version = '0.3(svn)'
 
 doc_dir = os.path.join(os.path.dirname(__file__), 'docs')
 index_filename = os.path.join(doc_dir, 'index.txt')
@@ -11,11 +11,11 @@ setup(name='PoachEggs',
       description="Install a batch of packages at once",
       long_description=open(index_filename).read(),
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
       ],
-      keywords='workingenv setuptools egg',
+      keywords='virtualenv setuptools egg',
       author='whit',
       author_email='whit@openplans.org',
       url='http://pypi.python.org/pypi/PoachEggs',
